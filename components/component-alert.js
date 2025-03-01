95de5cadaf8ba2d92ec743ac94387e504d9db9d6
samger <liw8172@gmail.com>
2025-03-01 11:52:05 +0800

    建立Vue3六角學院

---
+++ components/component-alert.js
@@ -0,0 +1,13 @@
+export default {
+    data() {
+        return {
+            text: '外部匯入的文字'
+        };
+    },
+    components:{
+    },
+    template: `<div class="alert alert-primary" role="alert">
+  {{ text }}
+    
+</div>`
+}
---
+++ es6/export2.js
@@ -0,0 +1,9 @@
+export const name = '王曉明';
+
+export function log(){
+    console.log(1);
+}
+
+export function total(c,d){
+    return c+d;
+}
